cb531684cf802a175b43c20223665095e44941a7
Yunhao Xiao <925180034@qq.com>
2025-07-09 09:40:14 +0800

    智能体构建

--- image-search-mcp-server/src/test/java/com/yunhao/imagesearchmcpserver/tools/ImageSearchToolTest.java
+++ image-search-mcp-server/src/test/java/com/yunhao/imagesearchmcpserver/tools/ImageSearchToolTest.java
@@ -4,8 +4,10 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")  // 激活 local profile
 public class ImageSearchToolTest {
 
     @Resource
